750ea521d4ec67ed1e6eca608d6adb36b6393a42
Erick De Belen <erickdebelen22@gmail.com>
2025-04-24 07:38:15 +0800

    Update for Prefinals

--- server.js
+++ server.js
@@ -1,51 +1,102 @@
-const express = require('express'); //install express
-const app = express(); // EXPRESS APP
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
+// MongoDB connection
+const dbURI = 'mongodb://localhost:27017/blogDB'; //papalitan ko ng url ng mongodb ko
+mongoose.connect(dbURI)
+  .then(() => app.listen(3000, () => console.log('Server running on port 3000')))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+// Middleware
 app.use(express.static('designs'));
-
-app.set('view engine', 'ejs') 
-
-//middleware
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-//Home Page
-app.get('/', (req,res) => {
-    
-    const blogs = [     
-        {title: 'CSDC105 ', snippet: 'Trends in Application Development'},
-        {title: 'RestAPI', snippet: 'Basic'},
-        {title: 'Finals', snippet: 'Sana perfect'},
-    ];
+// Blog Schema
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  snippet: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-    res.render('index', {title: "Home", blogs})
-})
+const Blog = mongoose.model('Blog', blogSchema);
 
-//About Page
-app.get('/about', (req,res) => {
-    res.render('about', {title: "About"})
-})
+// Routes
+// GET all blogs
+app.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.render('index', { title: 'Home', blogs });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching blogs');
+  }
+});
 
-//Redirect of about page
-app.get('/about-us', (req,res) => {
-    res.redirect('/about')
-})
+// About page
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-//Routing for create
-app.get('/blogs/create', (req,res) => {
-    res.render('create', {title: "Create"})
-})
-//create blog 
-app.post('/blogs', (req, res) => {
-    console.log(req.body); 
+// Redirect about-us to about
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// GET create blog form
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create Blog' });
+});
+
+// POST new blog
+app.post('/blogs', async (req, res) => {
+  try {
+    const blog = new Blog(req.body);
+    await blog.save();
     res.redirect('/');
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error creating blog');
+  }
+});
 
-//Page not FOUND
-app.use((req,res) => {
-    res.status(404).render('404', {title: "404"})
-})
+// GET single blog
+app.get('/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (blog) {
+      res.render('details', { title: blog.title, blog });
+    } else {
+      res.status(404).render('404', { title: '404' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching blog');
+  }
+});
 
-//Server
-app.listen(3000, () => {
-   console.log('Server is running on port 3000');
+// DELETE blog
+app.delete('/blogs/:id', async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.json({ redirect: '/' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Error deleting blog' });
+  }
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
